73583e7147ae16586697613e21fbbd2b9786f4db
Umair Farooq <mirumair7893@gmail.com>
2026-07-15 13:12:38 +0530

    message

---
+++ Conditional.py
@@ -0,0 +1,19 @@
+light = input("light color: ")
+
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("slow down")
+elif(light=="blue"):
+    print("gaadi side laga")
+else:    
+    print("light is broken")
+
+num = input("Number :")
+
+if num % 2 == 0:
+    print("Even Number")
+elif num == 0:
+    print("Number is zero")
+else:
+    print("Odd Number")
---
+++ loops.py
@@ -0,0 +1,23 @@
+myString = input("Enter a number: ")
+myInt = int(myString)
+
+for x in range(0, 5, 1):
+    print(myInt)
+
+for x in range(0, 5, 1):
+    print(x)
+
+for x in range(0,5,2):
+    print(x)
+
+
+count = 1 
+while count <=5:             
+    print(  "Hello")
+    count += 1  
+
+day = 0
+while day <=6:
+    print(day)
+    day=day+1
+
